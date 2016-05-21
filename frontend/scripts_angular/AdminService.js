be2d6e2014c41eef88df8dddbbf3992dd88d5600
Guillaume Serre <guillaume.serre@gmail.com>
2016-05-21 10:44:08 +0200

    Service must be authentified

--- frontend/scripts_angular/AdminService.js
+++ frontend/scripts_angular/AdminService.js
@@ -1,18 +1,18 @@
 angular.module('sports-predictions')
-    .factory('AdminService', ['$q', '$http', 'BackendService', 'Notification', function ($q, $http, BackendService, Notification) {
+    .factory('AdminService', ['$q', '$http', 'BackendService', 'UserService', 'Notification', function ($q, $http, BackendService, UserService, Notification) {
         return {
             getUsers: function () {
                 var deferredObject = $q.defer();
                 var result;
                 var config = {
-                    headers: { 'Accept': 'application/json' }
+                    headers: { 'Accept': 'application/json', 'Authorization' : 'Basic ' + UserService.getToken() }
                 };
 
                 $http.get(BackendService.getBackEndURL() + 'admin/users', config)
                     .then(function (response) {
                         deferredObject.resolve({ users: response.data });
                     }, function (response) {
-                        Notification.error('error');
+                        Notification.error( {'title':response.statusText, 'message':response.data} );
                     });
 
                 return deferredObject.promise;
