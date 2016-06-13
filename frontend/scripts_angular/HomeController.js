55d81f4821e21bfe0a3d3f691e7c4b30a5a65d66
LGouellec <sylvain.legouellec@hotmail.fr>
2016-06-13 22:48:43 +0200

    Add user rank next to profil link

--- frontend/scripts_angular/HomeController.js
+++ frontend/scripts_angular/HomeController.js
@@ -3,8 +3,12 @@
 * First controller
 * logOut() * Log out user connected
 **/
-angular.module('sports-predictions').controller('HomeController', ['$scope', '$location', 'UserService', 'BackendService', 'Notification', function ($scope, $location, UserService, BackendService, Notification) {
+angular.module('sports-predictions')
+.controller('HomeController', ['$scope', '$location', 'UserService', 'BackendService', 'Notification', 'RankingService', '$linq',
+function ($scope, $location, UserService, BackendService, Notification, RankingService, $linq) {
     
+	$scope.rank = 0;
+	
     $scope.logOut = function()	{
 		UserService.logout();
 		$location.path('/');
@@ -18,5 +22,23 @@ angular.module('sports-predictions').controller('HomeController', ['$scope', '$l
     $scope.admin = function() {
         return UserService.isAdmin();
     }
+	
+	$scope.init = function(){
+		RankingService.getRanks().then(
+			function( result ){
+				if (result.Ranks.RanksData != undefined){
+					$scope.rank = $linq.Enumerable()
+                            .From(result.Ranks.RanksData)
+                            .FirstOrDefault({currentRanking: 0}, function (rank) {
+                                return rank.email === UserService.getCurrentLogin();
+                            }).currentRanking;
+				}
+				else
+					Notification.error({message: result.Ranks.message, title: 'Erreur lors de la récupération du classement'});
+				}, function( reponse ) {
+					Notification.error( 'Position introuvable' );
+				}
+			)
+	}
 	
 }]);
