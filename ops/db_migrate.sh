16c4296d0a8d1316fc462f58d88e6b6786dfe3e3
serregu <guillaume.serre@cgi.com>
2018-04-30 13:42:55 +0200

    PostGreSQL support

---
+++ ops/db_migrate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -jar ./backend-1.0-SNAPSHOT.jar db migrate kimsufi-config.yml
---
+++ ops/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -jar ./backend-1.0-SNAPSHOT.jar server kimsufi-config.yml
