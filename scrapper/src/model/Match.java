e5c208f48a28299d89e408cb1d21d5a7dc9a0a75
serregu <guillaume.serre@cgi.com>
2018-05-06 10:53:08 +0200

    WC2018 scrapper and updated games.json

--- scrapper/src/model/Match.java
+++ scrapper/src/model/Match.java
@@ -1,67 +1,119 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.ZonedDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Match {
-	
-	private String dateTime;
-	private String group;
-	private String stadium;
-	private String homeTeam;
-	private String awayTeam;
-	
-	private int homeScore;
-	private int awayScore;
-	
+
+	@JsonProperty
 	private int matchNum;
 
-	public String getDateTime() {
-		return dateTime;
-	}
-	public void setDateTime(String dateTime) {
+	@JsonProperty
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm", timezone="GMT+2")
+	private ZonedDateTime dateTime;
+
+	@JsonProperty
+	private String group;
+
+	@JsonProperty
+	private String stadium;
+
+	@JsonProperty
+	private String homeTeamName;
+
+	@JsonProperty
+	private Integer homeTeamWinnerFrom;
+
+	@JsonProperty
+	private String awayTeamName;
+
+	@JsonProperty
+	private Integer awayTeamWinnerFrom;
+
+	public Match(int matchNum, ZonedDateTime dateTime, String group, String stadium, String homeTeamName, String awayTeamName) {
+		this.matchNum = matchNum;
 		this.dateTime = dateTime;
-	}
-	public String getGroup() {
-		return group;
-	}
-	public void setGroup(String group) {
 		this.group = group;
+		this.stadium = stadium.trim();
+		this.homeTeamName = homeTeamName.trim();
+		this.awayTeamName = awayTeamName.trim();
 	}
-	public String getStadium() {
-		return stadium;
-	}
-	public void setStadium(String stadium) {
+
+	public Match(int matchNum, ZonedDateTime dateTime, String group, String stadium, int homeTeamWinnerFrom, int awayTeamWinnerFrom) {
+		this.matchNum = matchNum;
+		this.dateTime = dateTime;
+		this.group = group;
 		this.stadium = stadium;
+		this.homeTeamWinnerFrom = homeTeamWinnerFrom;
+		this.awayTeamWinnerFrom = awayTeamWinnerFrom;
 	}
-	public String getHomeTeam() {
-		return homeTeam;
-	}
-	public void setHomeTeam(String homeTeam) {
-		this.homeTeam = homeTeam;
-	}
-	public String getAwayTeam() {
-		return awayTeam;
-	}
-	public void setAwayTeam(String awayTeam) {
-		this.awayTeam = awayTeam;
-	}
+
 	public int getMatchNum() {
 		return matchNum;
 	}
+
 	public void setMatchNum(int matchNum) {
 		this.matchNum = matchNum;
 	}
-	public int getHomeScore() {
-		return homeScore;
-	}
-	public void setHomeScore(int homeScore) {
-		this.homeScore = homeScore;
-	}
-	public int getAwayScore() {
-		return awayScore;
-	}
-	public void setAwayScore(int awayScore) {
-		this.awayScore = awayScore;
-	}
-	
-	
 
+	public ZonedDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(ZonedDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getStadium() {
+		return stadium;
+	}
+
+	public void setStadium(String stadium) {
+		this.stadium = stadium;
+	}
+
+	public String getHomeTeamName() {
+		return homeTeamName;
+	}
+
+	public void setHomeTeamName(String homeTeamName) {
+		this.homeTeamName = homeTeamName;
+	}
+
+	public Integer getHomeTeamWinnerFrom() {
+		return homeTeamWinnerFrom;
+	}
+
+	public void setHomeTeamWinnerFrom(Integer homeTeamWinnerFrom) {
+		this.homeTeamWinnerFrom = homeTeamWinnerFrom;
+	}
+
+	public String getAwayTeamName() {
+		return awayTeamName;
+	}
+
+	public void setAwayTeamName(String awayTeamName) {
+		this.awayTeamName = awayTeamName;
+	}
+
+	public Integer getAwayTeamWinnerFrom() {
+		return awayTeamWinnerFrom;
+	}
+
+	public void setAwayTeamWinnerFrom(Integer awayTeamWinnerFrom) {
+		this.awayTeamWinnerFrom = awayTeamWinnerFrom;
+	}
 }
--- scrapper/src/wc2018/scrapper/WC2018Scrapper.java
+++ scrapper/src/wc2018/scrapper/WC2018Scrapper.java
@@ -1,37 +1,142 @@
 package wc2018.scrapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import model.Match;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WC2018Scrapper {
 
     public void scrapMatches() throws IOException {
 
-        Document doc = Jsoup.connect("http://www.fifa.com/worldcup/matches/").get();
+        Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/2018_FIFA_World_Cup").get();
 
-        ObjectMapper mapper = new ObjectMapper(); // create once, reuse
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
 
         List<Match> matches = new ArrayList<>();
 
-        Elements fixtures = doc.select(".fixture");
+        Elements fixtures = doc.select("[itemtype=http://schema.org/SportsEvent");
         for (Element fixture : fixtures) {
 
-            String dataId = fixture.attr("data-id");
+            /*
+            <div itemscope="itemscope" itemtype="http://schema.org/SportsEvent" style="clear: both; overflow: auto;">
+            <div class="mobile-float-reset" style="float: left; width: 15%; padding: 2px 0; overflow: auto;"><time style="display: block; overflow: auto;"><span class="mobile-float-reset" style="display: block; float: right;">16&nbsp;June&nbsp;2018<span style="display:none">&nbsp;(<span class="bday dtstart published updated">2018-06-16</span>)</span></span><span class="mobile-float-reset" style="display: block; clear: right; float: right;">13:00 <a href="/wiki/Moscow_Time" title="Moscow Time">MSK</a> (<a href="/wiki/UTC%2B03:00" title="UTC+03:00">UTC+3</a>)</span></time></div>
+            <table style="float: left; width: 61%; table-layout: fixed; text-align: center;">
+            <tbody><tr itemprop="name" style="vertical-align: top;">
+                <th style="width: 39%; text-align: right;" itemprop="homeTeam" itemscope="itemscope" itemtype="http://schema.org/SportsTeam">
+                <span itemprop="name"><a href="/wiki/France_national_football_team" title="France national football team">France</a><span class="flagicon">&nbsp;<img alt="" src="//upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/23px-Flag_of_France.svg.png" width="23" height="15" class="thumbborder" srcset="//upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/35px-Flag_of_France.svg.png 1.5x, //upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/45px-Flag_of_France.svg.png 2x" data-file-width="900" data-file-height="600"></span></span></th>
+            <th style="width: 22%;"><a href="/wiki/2018_FIFA_World_Cup_Group_C#France_vs_Australia" title="2018 FIFA World Cup Group C">Match 5</a></th>
+            <th style="width: 39%; text-align: left;" itemprop="awayTeam" itemscope="itemscope" itemtype="http://schema.org/SportsTeam"><span itemprop="name"><span style="white-space:nowrap"><span class="flagicon"><img alt="" src="//upload.wikimedia.org/wikipedia/en/thumb/b/b9/Flag_of_Australia.svg/23px-Flag_of_Australia.svg.png" width="23" height="12" class="thumbborder" srcset="//upload.wikimedia.org/wikipedia/en/thumb/b/b9/Flag_of_Australia.svg/35px-Flag_of_Australia.svg.png 1.5x, //upload.wikimedia.org/wikipedia/en/thumb/b/b9/Flag_of_Australia.svg/46px-Flag_of_Australia.svg.png 2x" data-file-width="1280" data-file-height="640">&nbsp;</span><a href="/wiki/Australia_national_soccer_team" title="Australia national soccer team">Australia</a></span></span></th>
+            </tr>
+            <tr style="vertical-align: top; font-size: 85%;">
+            <td style="text-align: right;"></td>
+            <td><a rel="nofollow" class="external text" href="https://www.fifa.com/worldcup/matches/round=275073/match=300331533/report.html">Report</a></td>
+            <td style="text-align: left;"></td>
+            </tr>
+            </tbody></table>
+            <div class="mobile-float-reset" style="float: left; font-size: 85%; width: 24%; padding: 2px 0;">
+            <div itemprop="location" itemscope="itemscope" itemtype="http://schema.org/Place"><span itemprop="name address"><a href="/wiki/Kazan_Arena" title="Kazan Arena">Kazan Arena</a>, <a href="/wiki/Kazan" title="Kazan">Kazan</a></span></div>
+            </div>
+            </div>
+            */
 
-            String dateStr = fixture.select(".fixture").text();
+            Element dateTime = fixture.select("time").first();
+            String date = dateTime.select(".dtstart").text();
+            LocalDate localDate = LocalDate.parse(date);
 
-            System.out.println(fixture);
+            String time = dateTime.select("span").last().text();
+            Matcher timeMatcher = Pattern.compile("(\\d+:\\d+) (\\w+) \\((.*)\\)").matcher(time);
+            LocalTime localTime = null;
+            ZoneId zone = null;
+            if (timeMatcher.matches()) {
+                localTime = LocalTime.parse(timeMatcher.group(1));
+                zone = ZoneId.of(timeMatcher.group(3));
+            }
+
+            ZonedDateTime zonedDateTime = ZonedDateTime.of(localDate, localTime, zone);
+
+            Element name = fixture.select("[itemprop=name]").first();
+
+            System.out.println("Parsing " + name.text() );
+
+
+            Element nameElement = name.select("th").get(1);
+            String matchName = nameElement.text();
+
+            String group = null;
+            Elements links = nameElement.select("a");
+            if (!(links).isEmpty()) {
+                String groupText = links.first().attr("title");
+                Matcher matcher = Pattern.compile("2018 FIFA World Cup (.+)").matcher(groupText);
+                if (matcher.matches()) {
+                    group = matcher.group(1);
+                    if (group.startsWith("Group ")) {
+                        group = group.substring(6);
+                    }
+                }
+            }
+
+
+            Integer id = null;
+            Matcher matcher = Pattern.compile("Match (\\d+)").matcher(matchName);
+            if (matcher.matches()) {
+                id = Integer.parseInt( matcher.group(1) );
+            }
+
+            Element homeTeam = name.select("[itemprop=hometeam]").first();
+            String homeTeamName = homeTeam.select("[itemprop=name] a").text();
+            Elements homeFlag = homeTeam.select(".flagicon img");
+            if (!homeFlag.isEmpty()) {
+                String homeFlagSet = homeFlag.first().attr("srcset");
+            }
+
+            Element awayTeam = name.select("[itemprop=awayteam]").first();
+            String awayTeamName = awayTeam.select("[itemprop=name] a").text();
+            Elements awayFlag = awayTeam.select(".flagicon img");
+            if (!awayFlag.isEmpty()) {
+                String awayFlagSet = awayFlag.first().attr("srcset");
+            }
+
+            String location = fixture.select("[itemprop=location]").text();
+
+            // String dateTimeStr = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mmZ").format(zonedDateTime);
+
+            Match match;
+
+            Pattern winnersMatchPattern = Pattern.compile("Winners Match (\\d+)");
+            Matcher homeMatcher = winnersMatchPattern.matcher(homeTeamName);
+            Matcher awayMatcher = winnersMatchPattern.matcher(awayTeamName);
+            if (homeMatcher.matches() && awayMatcher.matches()) {
+                match = new Match(id, zonedDateTime, group, location, Integer.parseInt(homeMatcher.group(1)), Integer.parseInt(awayMatcher.group(1)));
+            } else {
+                match = new Match(id, zonedDateTime, group, location, homeTeamName, awayTeamName);
+            }
+
+            matches.add(match);
 
         }
-        System.out.println( mapper.writeValueAsString( matches ) );
+        Path path = Paths.get("games.json");
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path)) {
+            bufferedWriter.write(mapper.writeValueAsString( matches ));
+        }
     }
 
     public static void main(String[] argv) throws IOException {
