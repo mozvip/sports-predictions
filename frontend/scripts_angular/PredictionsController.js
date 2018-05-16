afd733a532ab04e2773a7aade8424bcb1d5d735e
serregu <guillaume.serre@cgi.com>
2018-05-16 08:49:58 +0200

    Fix missing groups

--- frontend/scripts_angular/PredictionsController.js
+++ frontend/scripts_angular/PredictionsController.js
@@ -14,7 +14,9 @@ function ($scope, $location, UserService, PredictionService, GamesService, Notif
         { id: 3, title: 'Groupe C', content: '<div class="GroupeC"><pronostic access="community.groupsAccess" ng-repeat="match in games | filter:{group:\'C\'}" match="match"></pronostic></div>' },
         { id: 4, title: 'Groupe D', content: '<div class="GroupeD"><pronostic access="community.groupsAccess" ng-repeat="match in games | filter:{group:\'D\'}" match="match"></pronostic></div>' },
         { id: 5, title: 'Groupe E', content: '<div class="GroupeE"><pronostic access="community.groupsAccess" ng-repeat="match in games | filter:{group:\'E\'}" match="match"></pronostic></div>' },
-        { id: 6, title: 'Groupe F', content: '<div class="GroupeF"><pronostic access="community.groupsAccess" ng-repeat="match in games | filter:{group:\'F\'}" match="match"></pronostic></div>' }];
+        { id: 6, title: 'Groupe F', content: '<div class="GroupeF"><pronostic access="community.groupsAccess" ng-repeat="match in games | filter:{group:\'F\'}" match="match"></pronostic></div>' },
+        { id: 7, title: 'Groupe G', content: '<div class="GroupeF"><pronostic access="community.groupsAccess" ng-repeat="match in games | filter:{group:\'G\'}" match="match"></pronostic></div>' },
+        { id: 8, title: 'Groupe H', content: '<div class="GroupeF"><pronostic access="community.groupsAccess" ng-repeat="match in games | filter:{group:\'H\'}" match="match"></pronostic></div>' }];
 
     $scope.init = function () {
         var error = false;
