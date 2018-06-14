7b24aac209183c1db47768e4957744850f7b9fde
serregu <guillaume.serre@cgi.com>
2018-06-14 20:52:42 +0200

    Ensure predictions can not be saved based on community settings

--- predictor/src/main/java/predictions/resources/UserResource.java
+++ predictor/src/main/java/predictions/resources/UserResource.java
@@ -167,6 +167,14 @@ public class UserResource {
 		Date now = new Date();
 		for (MatchPrediction prediction : predictions.getMatch_predictions_attributes()) {
 			Game game = gamesManager.getGame(prediction.getMatch_id());
+
+			if (game.getGroup() != null && !community.getGroupsAccess().equals(AccessType.W)) {
+				continue;
+			}
+			if (game.getRound() != null && !community.getFinalsAccess().equals(AccessType.W)) {
+				continue;
+			}
+
 			boolean saveMatch = (forceSave || game.getDateTime().after(now));
 			if (saveMatch) {
 				if (prediction.getHome_team_name() != null && prediction.getAway_team_name() != null) {
